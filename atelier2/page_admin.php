9be873e276388f32a3ee1afb484ddf675f7a47d6
Evan-hn27 <evho63465@eleve.isep.fr>
2025-12-11 11:44:47 +0100

    Update page_admin.php

--- atelier2/page_admin.php
+++ atelier2/page_admin.php
@@ -1,10 +1,8 @@
 <?php
-// Démarrer la session
 session_start();
 
-// Vérifier si l'utilisateur est bien en possession d'un cookie valide
-// Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
-if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== '12345') {
+// Vérifier le cookie et le token de session
+if (!isset($_COOKIE['authToken']) || !isset($_SESSION['authToken']) || $_COOKIE['authToken'] !== $_SESSION['authToken']) {
     header('Location: index.php');
     exit();
 }
@@ -15,10 +13,10 @@ if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== '12345') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+    <title>Page Admin</title>
 </head>
 <body>
-    <h1>Bienvenue sur la page Administrateur protégée par un Cookie</h1>
+    <h1>Bienvenue sur la page Administrateur</h1>
     <p>Vous êtes connecté en tant qu'admin.</p>
     <a href="logout.php">Se déconnecter</a>
 </body>
