8992ae075f3c4eb5ee1660d1b3d00ead554a7c56
Evan-hn27 <evho63465@eleve.isep.fr>
2025-12-11 11:56:56 +0100

    Update logout.php

--- atelier3/logout.php
+++ atelier3/logout.php
@@ -1,12 +1,11 @@
 <?php
-// Démarrer la session
 session_start();
 
 // Détruire la session
 session_unset();
 session_destroy();
 
-// Rediriger vers la page de connexion
+// Redirection vers la page de connexion
 header('Location: index.php');
 exit();
 ?>
